0747aa5fe33051b257646b9a0b302fa6e100e45d
epittab <ep7m@me.com>
2020-06-01 17:43:23 -0400

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,23 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(string) {
+  let hhMMArray = string.split(":");
+
+  const hours = parseInt(hhMMArray[0], 10);
+
+  if (hours < 12) {
+    return "Good Morning"
+  } else if (hours < 17) {
+    return "Good Afternoon"
+  } else {
+    return "Good Evening"
+  }
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(string) {
+  let greetingNode = document.querySelector("h1#greeting")
+  greetingNode.innerText = string;
+}
